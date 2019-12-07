1165c02ecd3e47f210b086d855b896731a534a0e
mnehrebecki <mati9817@gmail.com>
2019-12-07 22:15:49 +0100

    Login layout done

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -1,12 +1,22 @@
 <template>
     <login-layout>
-        <div class="home">
-             <form>
-                <input type="text"><br>
-                <input type="password"><br>
-                <input type="submit" value="Submit">
-            </form>
-        </div>
+        <b-container fluid>
+            <b-row class="my-1">
+                 <b-col class="col-sm-9 col-md-7 col-lg-4 mx-auto">
+                     <b-card title="Logowanie">
+                        <form>
+                            <b-col class="my-2">
+                            <b-form-input type="email" placeholder="Wpisz e-mail"></b-form-input>
+                            </b-col>
+                            <b-col class="my-2">
+                            <b-form-input type="password" placeholder="Wpisz hasło"></b-form-input>
+                            </b-col>
+                            <b-button type="submit" variant="primary">Zaloguj się</b-button>
+                        </form>
+                     </b-card>
+                 </b-col>
+            </b-row>
+        </b-container>
     </login-layout>
 </template>
 
