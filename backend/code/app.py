bec28af2445a73a8a4bf57248f61c51cdce07282
bartek <thebalowen@gmail.com>
2019-12-07 20:15:07 +0100

    fix before deploying to heroku

--- backend/code/app.py
+++ backend/code/app.py
@@ -10,8 +10,14 @@ from resources.user import UserRegister
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False    # turns off flask_sqlalchemy modification tracker
+app.config['PROPAGATE_EXCEPTIONS'] = True
 app.secret_key = 'bart'
 api = Api(app)
+
+# TODO usun przed wrzuceniem na heroku
+# @app.before_first_request
+# def create_tables():
+#     db.create_all()
 
 jwt = JWT(app, authenticate, identity)  # creates /auth endpoint
 
